b03c6f2c29b6db6da5aeb6aa5f059a3a4bc150d4
clauadser <clauadser@gmail.com>
2023-02-04 19:09:17 +0800

    First build

---
+++ README.md
@@ -0,0 +1,10 @@
+## Cs Scroll Lock JS
+============
+
+Javascript plugin for lock web page.
+
+This is a free source that can be used in your projects.
+
+Copyright © 2022 Clau Adser
+
+[https://clau.my.id](https://clau.my.id)
---
+++ cs.scrolllock.js
@@ -0,0 +1,78 @@
+/*
+ * cs.scrolllock JS 1.0
+ * Date: 2022-12-26
+ * Free Source Code
+ * Copyright © 2022 Clau Adser.
+ * https://clau.my.id
+ * 
+ */
+
+let html = document.querySelector("html"),
+	body = document.querySelector("body"),
+	offsetTop,
+	scrollTop,
+	lockStyle = {
+		position : "fixed",
+		left : "0",
+		width : "100%",
+		overflowY : "scroll"
+	},
+	unlockStyle = {
+		position : "",
+		top : "",
+		left : "",
+		width : "",
+		overflowY : ""
+	};
+
+const scrollLock = () => {
+	scrollTop = window.pageYOffset;
+
+	if (body.scrollHeight > html.clientHeight) {
+		if (window.pageYOffset) {
+			html.style.top = - scrollTop + "px";
+		}
+
+		Object.assign(html.style, lockStyle);
+
+		body.classList.add("locked");
+	}
+
+	offsetTop = parseInt(html.style.top.replace("-", "").replace("px", ""));
+
+	window.addEventListener("resize", () => {
+		if (!body.classList.contains("locked")) {
+			return;
+		}
+		
+		let contentHeight = body.scrollHeight,
+			top = contentHeight - html.clientHeight,
+			range = contentHeight - offsetTop;
+			
+		if (body.scrollHeight > html.clientHeight) {
+			if (offsetTop > 0) {
+				if (range > html.clientHeight) {
+					html.style.top = - offsetTop + "px";
+				} else {
+					html.style.top = - top + "px";
+				}
+			}
+			
+			Object.assign(html.style, lockStyle);
+		} else {			
+			Object.assign(html.style, unlockStyle);
+		}
+	});
+}
+
+const scrollUnlock = () => {
+	offsetTop = parseInt(html.style.top.replace("-", "").replace("px", ""));
+
+	if (body.classList.contains("locked")) {
+		Object.assign(html.style, unlockStyle);
+
+		window.scrollTo(0, offsetTop);
+
+		body.classList.remove("locked");
+	}
+}
